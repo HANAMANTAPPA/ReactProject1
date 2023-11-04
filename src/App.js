a8caf6611c6542fcfb31f5ee09fece5bd1bf48dd
unknown <hanamantappak26@gmail.com>
2023-11-04 16:31:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './icon1.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          hi I'm Hanumanth
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
