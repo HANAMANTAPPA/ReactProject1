398674c7c456e7f546773551783974ae56a1ed90
unknown <hanamantappak26@gmail.com>
2023-11-05 21:14:21 +0530

    added Increase function

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,7 +9,12 @@ class CartItems extends React.Component{
             qty: 1,
             img : ''
         }
-
+        // this.increaseQuantity =this.increaseQuantity.bind(this);
+    }
+    // function for handling events 
+     increaseQuantity= () => {
+        // (this.state.qty )++; 
+        console.log("This ", this.state.qty); 
     }
     render (){
         const { price , title , qty } =this.state;
@@ -17,16 +22,24 @@ class CartItems extends React.Component{
             <div className="cart-item"> 
                 {/* <h1>HI I'm div content for CartItem </h1>    */}
                 <div className ="left-box">
-                    <img style={style1.image} src="" />  
+                    <img style={style1.image} 
+                    src="https://as2.ftcdn.net/v2/jpg/01/07/62/07/1000_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg"  alt=""/>  
                 </div>
                 <div className="right-box">
                     <div style={ {fontSize: 25} }> {title} </div>
                     <div style={{color: '#7777' }}>Rs {price} </div>
                     <div style={{color: '#7777' }}>Quantity: {qty} </div>
                     <div className="cart-item-action">
-                        <img alt="increase" className="action-icons" src="https://as2.ftcdn.net/v2/jpg/01/07/62/07/1000_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" />  
-                        <img alt="decrease" className="action-icons" src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" /> 
-                        <img alt="delete" className="action-icons" src="https://as2.ftcdn.net/v2/jpg/03/73/50/09/1000_F_373500918_7vISJB85YXvvu7SgnpktP752LWRrLzyI.jpg" /> 
+                        <img alt="increase" 
+                        onClick={this.increaseQuantity}
+                        className="action-icons" 
+                        src="https://as2.ftcdn.net/v2/jpg/01/07/62/07/1000_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" />  
+                        <img alt="decrease" 
+                        className="action-icons" 
+                        src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" /> 
+                        <img alt="delete" 
+                        className="action-icons" 
+                        src="https://as2.ftcdn.net/v2/jpg/03/73/50/09/1000_F_373500918_7vISJB85YXvvu7SgnpktP752LWRrLzyI.jpg" /> 
                     </div>
                 </div>
             </div>
