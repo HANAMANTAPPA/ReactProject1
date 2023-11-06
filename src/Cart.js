71e0b61c435590e0e12b11f7eed95dfffcf7f768
unknown <hanamantappak26@gmail.com>
2023-11-06 20:44:49 +0530

    Cart-wrap

---
+++ src/Cart.js
@@ -0,0 +1,22 @@
+import React from "react";
+import CartItem from './CartItem';
+
+class Cart extends React.Component{
+    
+    
+    render (){ 
+        return (
+            <div className="cart-wrap">  
+             <CartItem />
+             <CartItem />
+             <CartItem />
+             <CartItem />
+             <CartItem />
+             <CartItem />
+            </div>
+        );
+    }
+}
+
+ 
+export default Cart;
